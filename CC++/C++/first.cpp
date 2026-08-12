3e0d91e7ca1c20447d348dfcf7ba5544a15f84ee
rajatagrawal01 <rajatagrawal01@gmail.com>
2026-08-12 13:51:05 +0530

    cpp variables , user io , arithmatic operators

--- CC++/C++/first.cpp
+++ CC++/C++/first.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main(){
-    cout<<"Hello World";
+    int age=23;
+    string name="Kailash";
+    int marks;
+
+    cout<<"Age = "<<age<<endl;
+    cout<<"Name ="<<name<<endl;
+
+    cout<<"Enter Your marks: ";
+    cin>>marks;
+    cout<<"Your marks are : "<< marks;
+
+
+    
     return 0;
 }
---
+++ CC++/C++/operators.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+   int a,b;
+   cout<<"Enter Your Numbers : ";
+   cin>>a>>b;
+
+   cout<<"The sum of a and b is : "<<a+b<<endl;
+   cout<<"The sub of a and b is : "<<a-b<<endl;
+   cout<<"The mul of a and b is : "<<a*b<<endl;
+   cout<<"The div of a and b is : "<<a/b<<endl;
+
+   
+   return 0;
+}
